1cb5a5590f393d456e1af66f25188213c1eed6c0
dsurguy <surguy.derek@gmail.com>
2017-01-25 19:21:48 +0000

    MinHeap logic, pop is not swapping correctly

--- script.js
+++ script.js
@@ -1,22 +1,100 @@
-function CustomerNode(serviceTime, left, right){
-    this.left = left||undefined;
-    this.right = right||undefined;
+function CustomerNode(serviceTime){
+    this.id;
     this.serviceTime = serviceTime||undefined;
-};
+}
 
 function MinHeap(size){
     this._nodes = [];
-    this._nodes.length = size;
+    this._nodes.length = size + 1; //1-based array
     this._count = 0;
-    this.head = this._nodes[0];
-};
+    this.head = this._nodes[1];
+}
 MinHeap.prototype.pop = function (){
-    count--;
-    //TODO: Actual Logic
+    //throw error if we can't pop
+    if( this.isEmpty() ){
+        throw new Error('MinHeap empty');
+    }
+    
+    //decrement total count
+    this._count--;
+    
+    //empty the head node and store it to return
+    var returnNode = this._nodes[1];
+    this._nodes[1] = undefined;
+    
+    //swap the lowest child into the head node
+    for( var i=this._nodes.length-1; i>0; i-- ){
+        if( this._nodes[i] !== undefined ){
+            this._nodes[0] = this._nodes[i];
+            this._nodes[i] = undefined;
+            break;
+        }
+    }
+    
+    //check to see if we need to fix the heap
+    var curPosition = 1,
+        leftChild,
+        rightChild;
+        
+    var go = true;
+    while( go ){
+        var tempNode;
+        leftChild = curPosition*2;
+        rightChild = curPosition*2 + 1;
+        if( this._nodes[leftChild] && this._nodes[leftChild].serviceTime < this._nodes[curPosition] ){
+            //swap left
+            tempNode = this._nodes[curPosition];
+            this._nodes[curPosition] = this._nodes[leftChild];
+            this._nodes[leftChild] = tempNode;
+            curPosition = leftChild;
+        }
+        else if( this._nodes[rightChild] && this._nodes[rightChild].serviceTime < this._nodes[curPosition] ){
+            //swap right
+            tempNode = this._nodes[curPosition];
+            this._nodes[curPosition] = this._nodes[rightChild];
+            this._nodes[rightChild] = tempNode;
+            curPosition = rightChild;
+        }
+        else{
+            //done swapping
+            go = false;
+        }
+    }
+    
+    return returnNode;
 };
-MinHeap.prototype.push = function (node){
-    count++;
-    //TODO: Actual Logic
+MinHeap.prototype.push = function (newNode){
+    //throw error if we can't push
+    if( this.isFull() ){
+        throw new Error('MinHeap full');
+    }
+    //increment the total node count
+    this._count++;
+    //add the new node at the first available position in the heap
+    var curPosition = 1;
+    for( var i=1; i<this._nodes.length; i++ ){
+        if( this._nodes[i] === undefined ){
+            this._nodes[i] = newNode;
+            curPosition = i;
+            break;
+        }
+    }
+    //check to see if we need to fix the heap
+    var parentPosition;
+    var go = true;
+    while( curPosition > 1 && go ) {
+        parentPosition = Math.floor(curPosition/2);
+        if( this._nodes[curPosition].serviceTime < this._nodes[parentPosition].serviceTime ){
+            //swap parent and child
+            var tempNode = this._nodes[parentPosition];
+            this._nodes[Math.floor(curPosition/2)] = this._nodes[curPosition];
+            this._nodes[curPosition] = tempNode;
+            curPosition = parentPosition;
+        }
+        else{
+            go = false;
+        }
+    }
 };
 MinHeap.prototype.each = function (callback){
     for(var i=0; i<this.nodes.length; i++){
@@ -26,11 +104,14 @@ MinHeap.prototype.each = function (callback){
 MinHeap.prototype.isFull = function (){
     return this._count == this._nodes.length;
 };
+MinHeap.prototype.isEmpty = function (){
+    return this._count == 0;  
+};
 
 function Bank(numTellers){
     this.heap = new MinHeap(numTellers);
     this.processedCount = 0;
-};
+}
 /**
 *   Pop the current customer (with the least time) off the top of the heap,
 *    and then progress the service time of all other customers by the amount
